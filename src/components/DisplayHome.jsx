8fc752c99344adafdde4513652479691caad5c20
Hoa JP <itlmh2k3@gmail.com>
2025-04-18 10:23:11 +0700

    create sidebar, music player, album list and song list

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData, songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              id={item.id}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hit</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              id={item.id}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
